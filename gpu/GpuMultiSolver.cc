19080d4fc12c4f0eb9b927760c2779e597be34b4
Nicolas Prevot <nicolasprevot2@gmail.com>
2020-12-24 09:20:18 +0000

    fix printing final stats

--- gpu/GpuMultiSolver.cc
+++ gpu/GpuMultiSolver.cc
@@ -174,7 +174,8 @@ lbool GpuMultiSolver::solve(int _cpuThreadCount) {
         }
     }
     if (verb.global > 0) {
-        SYNCED_OUT(printf("c printing final stats\n"));
+        SyncOut so;
+        printf("c printing final stats\n");
         printStats();
     }
 
