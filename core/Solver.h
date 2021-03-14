d8c988a985f3926fa7e96483981e8ed77b5409fe
Mate Soos <soos.mate@gmail.com>
2021-03-14 23:38:48 +0100

    Adding missing string header

--- core/Solver.h
+++ core/Solver.h
@@ -70,6 +70,7 @@ OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWA
 #include "core/Constants.h"
 #include "mtl/Clone.h"
 #include <map>
+#include <string>
 
 // like printf, but two threads won't call it at the same time
 #define SyncedOutPrintf(...) {\
